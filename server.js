2e96f933372817fe98b2260d2c1ca67115434e83
Emmanuel DK Dolo <emmanueldkdolo199@gmail.com>
2024-01-24 23:13:18 +0000

    added dynamic pages

--- server.js
+++ server.js
@@ -1,41 +1,25 @@
-/* ** * This server.js file is the primary file of the 
- * application. It is used to control the project.
- *******************************************/
-/* ***********************
- * Require Statements
- *************************/
+require('dotenv').config();
+ 
+// Require Statements
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
-const env = require("dotenv").config();
 const app = express();
-const staticRoutes = require("./routes/static");
+const staticRoutes = require("./routes/static"); 
+const baseController = require("./controllers/baseController"); 
+const inventoryRoute = require("./routes/inventoryRoute");
 
-/* ***********************
- * View Engine and Templates 
- *************************/
 app.set("view engine", "ejs");
 app.use(expressLayouts);
-app.set("layout", "./layouts/layout"); // not at views root
+app.set("layout", "./layouts/layout");
 
-/* ***********************
- * Routes
- *************************/
 app.use(staticRoutes); 
-// index route 
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"});
-});
+// index routes 
+app.use("/inv", inventoryRoute);
+app.get("/", baseController.buildHome); 
 
-/* ***********************
- * Local Server Information
- * Values from .env (environment) file
- *************************/
-const port = process.env.PORT || 3000; 
-const host = process.env.HOST || 'localhost'; 
+const port = process.env.PORT;
+const host = process.env.HOST;
 
-/* ***********************
- * Log statement to confirm server operation
- *************************/
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`);
 });
